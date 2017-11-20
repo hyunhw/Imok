9d414f2ce7e5d8128c326dd2a33c9387ee2350c0
Jennifer Hwang <hhwang0908@gmail.com>
2017-11-19 23:14:14 -0500

    working bcrypt hashin

--- server/models/user.js
+++ server/models/user.js
@@ -27,15 +27,38 @@ module.exports = (sequelize, DataTypes) => {
     };
 
     user.hook('beforeCreate', (user, options) => {
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(user.password, salt, null, function(err, hash) {
-                user.password = hash;
+        console.log('hook function \'before create\' is being hit!');
+        // bcrypt.genSalt(10, function(err, salt) {
+        //     bcrypt.hash(user.password, salt, null, function(err, hash) {
+        //         user.password = hash;
+        //     });
+        // });
+        return cryptPassword(user.password)
+            .then( hashed => {
+                user.password = hashed;
+            })
+            .catch( err => {
+                if (err) console.log(err);
             });
-        });
     });
     user.prototype.validPassword = function (password) {
-        // return bcrypt.compareSync(password, this.password);
-        return password === this.password;
+        return bcrypt.compareSync(password, this.password);
+        // return password === this.password;
     };
+
+    function cryptPassword(password) {
+        console.log('cryptPassword' + password);
+        return new Promise(function(resolve, reject) {
+            bcrypt.genSalt(10, function(err, salt) {
+                // Encrypt password using bycrpt module
+                if (err) return reject(err);
+
+                bcrypt.hash(password, salt, null, function(err, hash) {
+                    if (err) return reject(err);
+                    return resolve(hash);
+                });
+            });
+        });
+    }
     return user;
 };
